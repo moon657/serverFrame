f5320533574201e1c0e9efa49919021706c79446
liuwenxin07 <liuwenxin07@meituan.com>
2022-07-26 13:13:01 +0800

    Initial Commit

--- welcom.cc
+++ welcom.cc
@@ -1,30 +1,47 @@
 #include <iostream>
 #include "myVector.hpp"
-#include <chrono>
-#include <thread>
+#include <vector>
 void proc(){
     MyVec<int > vec(4);
-    vec.reserve(5);
+    vec.reserve(6);
     std::cout<<vec.size()<<std::endl;
     vec.push_back(1);
     vec.push_back(2);
-    for(int i = 0;i<8;i++){
+    for(int i = 0;i<10000000;i++){
         vec.push_back(i);
     }
-    for(int i = 0;i<vec.size();i++){
-        std::cout<<vec[i]<<' ';
+    std::cout<<std::endl;
+    auto p = new int[100000000];
+    *p = 1000;
+    int* mp = (int*)malloc(10);
+    //std::this_thread::sleep_for( std::chrono::seconds(1));
+    return;
+}
+void pro2(){
+    std::vector<int> vec;
+    vec.reserve(5);
+    for(int i = 0;i<100;i++){
+        vec.push_back(i);
     }
-    auto p = new int[10];
-    std::this_thread::sleep_for( std::chrono::seconds(5));
-
-
+    std::this_thread::sleep_for( std::chrono::seconds(2));
     return;
 }
 
+void newtest()
+{
+	int *p = new int[10]; //申请40字节
+	return;
+}
+
+void malloctest()
+{
+	int *q = (int*)malloc(10); //申请10字节
+	return;
+}
 int main(){
-    while (true){
-        proc();
-    }
+    malloctest();
+    newtest();
+    int  i=0;
     std::cout<<"   _____ __               __                  __"<<std::endl;
     std::cout<<"  / ___// /_  ___  ____  / /_  ___  _________/ /"<<std::endl;
     std::cout<<"  \\__ \\/ __ \\/ _ \\/ __ \\/ __ \\/ _ \\/ ___/ __  / "<<std::endl;
